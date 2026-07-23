165d9f9331e7ba392f56605cc76dabf70ab637ff
Yohannes Belete <130431688+yemijohnnyaladel@users.noreply.github.com>
2026-07-23 16:01:19 +0300

    dictionary update

---
+++ location.py
@@ -0,0 +1,12 @@
+customer = {
+    "name": "Almaz bekele",
+    "balance": 1500,
+    "city": "Addis Ababa",
+}
+
+print(customer["name"])
+
+customer["balance"] = 2000
+
+print(f"Customer name: {customer['name']}")
+print(f"Customer balance: {customer['balance']}")
--- print.py
+++ print.py
@@ -1,3 +1,6 @@
 names=["Almaz","Dawit","Tigist"]
 for name in names:
     print(f"selam,{name}")
+    for i,name in enumerate(name, 1):
+        print(f"{i}.{name}")
+        
